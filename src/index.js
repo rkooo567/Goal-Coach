e9aa50adb9266ab35d9b1a57f2fa8644218244c1
rkooo567 <rkooo567@gmail.com>
2018-01-05 23:03:58 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+import App      from './App';
+import SignIn   from './SignIn';
+import SignUp   from './SignUp';
+
+ReactDOM.render(
+  <div>App</div>,
+  document.getElementById('root')
+);
